baf4cf4c07db5d07c79c4afc024b22e7ccd5f011
zbenii <besic.beni@gmx.at>
2022-02-01 12:18:05 +0100

    #leocode-3 done

--- testing-api/src/main/java/at/htl/control/FileHandler.java
+++ testing-api/src/main/java/at/htl/control/FileHandler.java
@@ -24,7 +24,7 @@ public class FileHandler {
     private final Path BUILD_RESULT = Paths.get("../result.txt");
     private final Path RUN_TEST_SCRIPT = Paths.get("../run-tests.sh");
     private final List<String> SHELL_SCRIPT_CONTENT = Arrays.asList("cd " + PROJECT_UNDER_TEST_DIRECTORY.toString(),
-            "/opt/jenkinsfile-runner/bin/jenkinsfile-runner -w /opt/jenkins -p /opt/jenkins_home/plugins/ -f ./Jenkinsfile > log.txt",
+            "docker run  -v "+ PROJECT_UNDER_TEST_DIRECTORY.toString() + ":/workspace/ ppiper/jenkinsfile-runner > log.txt"",
             "tail -n 1 log.txt > " + BUILD_RESULT.toString());
 
 
