6593fa24be14c9f382896ced22e335b390eff62b
zbenii <besic.beni@gmx.at>
2022-03-15 09:49:42 +0100

    changes

--- leocode-backend/src/main/java/at/htl/boundary/SubmissionEndpoint.java
+++ leocode-backend/src/main/java/at/htl/boundary/SubmissionEndpoint.java
@@ -121,7 +121,7 @@ public class SubmissionEndpoint {
     @Path("/{id}")
     @Produces(MediaType.SERVER_SENT_EVENTS)
     @SseElementType("text/plain")
-    @Transactional(Transactional.TxType.MANDATORY)
+    @Transactional()
     public void stream(@Context Sse sse, @Context SseEventSink sseEventSink, @PathParam("id") Long id) {
         Submission currentSubmission = submissionRepository.findById(id);
         boolean canSubscribe = false;
